d39e7a9a6e122e83a9c6782297a545336a0de2ec
Emanuel Escudero <escudero.mdz@gmail.com>
2023-07-01 00:56:48 -0300

    SQL Server Practice Repository

---
+++ SQLQuery(Paises-Estados).sql
@@ -0,0 +1,16 @@
+INSERT INTO Paises (Nombre) VALUES ('Argentina')
+
+SELECT * FROM Paises
+
+INSERT INTO Estados 
+(Nombre, Pais_Id) VALUES ('Córdoba', 1)
+
+SELECT * FROM Paises
+SELECT * FROM Estados
+
+DELETE FROM Estados WHERE Estado_Id=4
+
+DELETE FROM Paises 
+WHERE Pais_Id = 1
+
+
